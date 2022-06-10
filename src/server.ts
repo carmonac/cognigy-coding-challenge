869f95e5a6df5ada0ec652a3a34e38cd563caaa6
Carlos Merchán Carmona <ccarmona.87@outlook.es>
2022-06-10 23:50:32 +0200

    Add global middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import { ServerApp } from "./app";
 import config from "./config";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 let app: ServerApp;
 
 export const init = async () => {
     app = new ServerApp({
-        globalMiddleware: [],
+        globalMiddleware: [
+            compression(),
+            helmet(),
+            cors(),
+            morgan(config.morganFormat),
+        ],
         Controllers: [],
         Services: []
     });
